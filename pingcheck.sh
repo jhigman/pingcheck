09e687fe076c3ae7175c33a8c1f1b8f1df2f8ce0
Jullian Higman <jhigman@pobox.com>
2014-06-10 11:58:33 +0100

    Show progress dots while OK, and datetime when fail.

--- pingcheck.sh
+++ pingcheck.sh
@@ -4,10 +4,12 @@ do
 ping -c 1 google.com > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
+	echo "Ping failed at $(date)"
 	notify-send "Ping failed at $(date)"
 	paplay /usr/share/sounds/ubuntu/stereo/phone-outgoing-busy.ogg 
 	sleep 10
 else
+	printf "."
 	sleep 3
 fi
 done
